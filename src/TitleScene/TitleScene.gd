5c15be3e5fd107017edbe52a6e280236c690cde8
Evan Lam <45635779+IMlame@users.noreply.github.com>
2022-08-18 14:01:37 -0700

    done (for now)

---
+++ src/TitleScene/TitleScene.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+
+var titleScreenLabel = load("res://src/TitleScene/TitleScreenButton.tscn")
+var screen_size = null
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+	var start_game_label = titleScreenLabel.instance()
+	start_game_label.position = Vector2(_percent_to_pixels_width(5), _percent_to_pixels_height(55))
+	start_game_label.init("Start Game")
+	add_child(start_game_label)
+	start_game_label.connect("title_screen_button_pushed", self, "_on_button_pushed")
+	
+	var settings_label = titleScreenLabel.instance()
+	settings_label.position = Vector2(_percent_to_pixels_width(5), _percent_to_pixels_height(70))
+	settings_label.init("Settings")
+	add_child(settings_label)
+	settings_label.connect("title_screen_button_pushed", self, "_on_button_pushed")
+	
+	var quit_label = titleScreenLabel.instance()
+	quit_label.position = Vector2(_percent_to_pixels_width(5), _percent_to_pixels_height(85))
+	quit_label.init("Quit")
+	add_child(quit_label)
+	quit_label.connect("title_screen_button_pushed", self, "_on_button_pushed")
+
+func _on_button_pushed(button_name: String):
+	match button_name:
+		"Start Game":
+			print("start game")
+		"Settings":
+			print("settings")
+		"Quit":
+			get_tree().quit()
+			
+func _percent_to_pixels_width(percent: float):
+	return screen_size.x * percent * .01
+	
+func _percent_to_pixels_height(percent: float):
+	return screen_size.y * percent * .01
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ src/TitleScene/TitleScreenButton.gd
@@ -0,0 +1,14 @@
+extends Label
+
+
+#func init(title: String):
+#	print("hi " + title)
+#	text = title
+	
+func _ready():
+	print('ready')
+	pass
+
+
+func _process(delta):
+	pass	
