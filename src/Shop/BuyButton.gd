8d200badc743993d6762f510f8df74bc2858eec5
Peter Oh <lovenchan01@gmail.com>
2022-09-08 03:40:16 -0700

    Shop version 2 without card

--- src/Shop/BuyButton.gd
+++ src/Shop/BuyButton.gd
@@ -1,8 +1,15 @@
 extends Button
 
-var count = 0
+var count: int = 0		# Keeping track of buy count
+var price: int = 5		# Starting price
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 	
+func _on_BuyButton_pressed():
+	# If bought less than 3 times then return price, else return 0 as null
+	if count < 3:
+		emit_signal("pressed", price)
+	else:
+		emit_signal("pressed", 0)
--- src/Shop/Shop.gd
+++ src/Shop/Shop.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var gold = 50	
+var gold = 105
 
 func _ready():
 	randomize();
@@ -11,63 +11,63 @@ func set_gold(gold):
 	self.gold = gold
 	$Gold.text = gold as String
 
-func _on_BuyButton1_pressed():
-	match $BuyButton1.count:
-		0:
-			if gold >= 5:
-				gold -= 5
-				$BuyButton1.count += 1
-				$BuyButton1.text = "Buy 10"
-		1:
-			if gold >= 10:
-				gold -= 10
-				$BuyButton1.count += 1
-				$BuyButton1.text = "Buy 20"
-		2:
-			if gold >= 20:
-				gold -= 20
-				$BuyButton1.count += 1
-				$BuyButton1.text = "Sold out"
+#func _on_BuyButton1_pressed():
+#	match $BuyButton1.count:
+#		0:
+#			if gold >= 5:
+#				gold -= 5
+#				$BuyButton1.count += 1
+#				$BuyButton1.text = "Buy 10"
+#		1:
+#			if gold >= 10:
+#				gold -= 10
+#				$BuyButton1.count += 1
+#				$BuyButton1.text = "Buy 20"
+#		2:
+#			if gold >= 20:
+#				gold -= 20
+#				$BuyButton1.count += 1
+#				$BuyButton1.text = "Sold out"
+#
+#	$Gold.text = gold as String
+
+func _on_BuyButton1_pressed(price: int):
+	# If price isn't null (0), then buy card and update the gold
+	if price > 0 and gold >= price:
+		gold -= price
+		$Gold.text = gold as String
+		$BuyButton1.count += 1
+		if $BuyButton1.count < 3:
+			$BuyButton1.price *= 2
+			$BuyButton1.text = "Buy " + $BuyButton1.price as String
+		else:
+			$BuyButton1.text = "Sold out"
+
+func _on_BuyButton2_pressed(price: int):
+	# If price isn't null (0), then buy card and update the gold
+	if price > 0 and gold >= price:
+		gold -= price
+		$Gold.text = gold as String
+		$BuyButton2.count += 1
+		if $BuyButton2.count < 3:
+			$BuyButton2.price *= 2
+			$BuyButton2.text = "Buy " + $BuyButton1.price as String
+		else:
+			$BuyButton2.text = "Sold out"
 				
 	$Gold.text = gold as String
 
-func _on_BuyButton2_pressed():
-	match $BuyButton2.count:
-		0:
-			if gold >= 5:
-				gold -= 5
-				$BuyButton2.count += 1
-				$BuyButton2.text = "Buy 10"
-		1:
-			if gold >= 10:
-				gold -= 10
-				$BuyButton2.count += 1
-				$BuyButton2.text = "Buy 20"
-		2:
-			if gold >= 20:
-				gold -= 20
-				$BuyButton2.count += 1
-				$BuyButton2.text = "Sold out"
-				
-	$Gold.text = gold as String
+
+func _on_BuyButton3_pressed(price: int):
+	# If price isn't null (0), then buy card and update the gold
+	if price > 0 and gold >= price:
+		gold -= price
+		$Gold.text = gold as String
+		$BuyButton3.count += 1
+		if $BuyButton3.count < 3:
+			$BuyButton3.price *= 2
+			$BuyButton3.text = "Buy " + $BuyButton1.price as String
+		else:
+			$BuyButton3.text = "Sold out"
 
 
-func _on_BuyButton3_pressed():
-	match $BuyButton3.count:
-		0:
-			if gold >= 5:
-				gold -= 5
-				$BuyButton3.count += 1
-				$BuyButton3.text = "Buy 10"
-		1:
-			if gold >= 10:
-				gold -= 10
-				$BuyButton3.count += 1
-				$BuyButton3.text = "Buy 20"
-		2:
-			if gold >= 20:
-				gold -= 20
-				$BuyButton3.count += 1
-				$BuyButton3.text = "Sold out"
-
-	$Gold.text = gold as String
