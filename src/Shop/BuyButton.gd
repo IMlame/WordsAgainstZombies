d0f5f76e2619e32d0b4ae17bd62be12f93653944
Peter Oh <lovenchan01@gmail.com>
2022-09-08 07:24:10 -0700

    Shop version 2
    signal bug fixed

--- src/Shop/BuyButton.gd
+++ src/Shop/BuyButton.gd
@@ -1,5 +1,7 @@
 extends Button
 
+signal my_price(price)
+
 var count: int = 0			# Keeping track of buy count
 var price: int = 5			# Starting price
 const MAX_COUNT: int = 3	# Maximum buy count
@@ -11,9 +13,9 @@ func _ready():
 func _on_BuyButton_pressed():
 	# If bought less than 3 times then return price, else return 0 as null
 	if count < MAX_COUNT:
-		emit_signal("pressed", price)
+		emit_signal("my_price", price)
 	else:
-		emit_signal("pressed", 0)
+		emit_signal("my_price", 0)
 
 func scale_price(scalar: int):
 	price *= scalar
--- src/Shop/Shop.gd
+++ src/Shop/Shop.gd
@@ -13,12 +13,6 @@ func _ready():
 	$Gold.text = gold as String
 	set_starting_price(starting_price)
 	
-#	var card = CARDBASE.instance()
-#	card.Cardname = POOL.CARDLIST[randi() % POOL.SIZE]
-#	card.rect_position = Vector2(500,500)
-#	$Cards.get_child(0).add_child(card)
-#	print($Cards.get_child(0).get_child(0).rect_position)
-	
 	# Initialize cards
 	for i in range(3):
 		var selected_card = POOL.CARDLIST[randi() % POOL.SIZE]
@@ -30,23 +24,26 @@ func _ready():
 			card.rect_position = Vector2(i * (card.get_node("Card").texture.get_size().x + 150) + 200, 
 									200 + card.get_node("Card/Bar/TopBar").rect_size.y * j)
 
-func _on_BuyButton1_pressed(price: int):
-	# If price isn't null (0), then buy card and update the gold
+# Update Player's gold based on price of the card
+func _on_BuyButton1_my_price(price):
 	if price > 0 and gold >= price:
 		set_gold(gold - price)
-		$Cards/Slot1.get_child(num_card - 1 - $BuyButton1.count).queue_free()
+		$Cards/Slot1.get_child(num_card - 1 - $BuyButton1.count).queue_free() # remove card
 		$BuyButton1.count += 1
 		if $BuyButton1.count < $BuyButton1.MAX_COUNT:
 			$BuyButton1.scale_price(price_scalar)
 			$BuyButton1.text = "Buy " + $BuyButton1.price as String
 		else:
 			$BuyButton1.text = "Sold out"
+				
+	$Gold.text = gold as String
 
-func _on_BuyButton2_pressed(price: int):
+# Update Player's gold based on price of the card
+func _on_BuyButton2_my_price(price):
 	# If price isn't null (0), then buy card and update the gold
 	if price > 0 and gold >= price:
 		set_gold(gold - price)
-		$Cards/Slot2.get_child(num_card - 1 - $BuyButton2.count).queue_free()
+		$Cards/Slot2.get_child(num_card - 1 - $BuyButton2.count).queue_free() # remove card
 		$BuyButton2.count += 1
 		if $BuyButton2.count < $BuyButton2.MAX_COUNT:
 			$BuyButton2.scale_price(price_scalar)
@@ -56,11 +53,12 @@ func _on_BuyButton2_pressed(price: int):
 				
 	$Gold.text = gold as String
 
-func _on_BuyButton3_pressed(price: int):
+# Update Player's gold based on price of the card
+func _on_BuyButton3_my_price(price):
 	# If price isn't null (0), then buy card and update the gold
 	if price > 0 and gold >= price:
 		set_gold(gold - price)
-		$Cards/Slot3.get_child(num_card - 1 - $BuyButton3.count).queue_free()
+		$Cards/Slot3.get_child(num_card - 1 - $BuyButton3.count).queue_free() # remove card
 		$BuyButton3.count += 1
 		if $BuyButton3.count < $BuyButton3.MAX_COUNT:
 			$BuyButton3.scale_price(price_scalar)
@@ -82,3 +80,7 @@ func set_starting_price(price: int):
 # Update price scalar of the shop
 func set_price_scalar(scalar: int):
 	price_scalar = scalar
+
+
+
+
