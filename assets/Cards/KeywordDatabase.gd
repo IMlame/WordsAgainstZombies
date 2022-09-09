d96baa50e1d443929d1d53be50955b621daea346
Peter Oh <lovenchan01@gmail.com>
2022-09-08 20:43:57 -0700

    Updated CardDatabase and added KeywordDatabase

---
+++ assets/Cards/KeywordDatabase.gd
@@ -0,0 +1,11 @@
+# Keyword : ["Against enemy", "Against player"]
+
+const DATA = {
+	"Burn" : ["Attacking results in self damage", "Cards require health to use"],
+	"Freeze" : ["Can't attack next move", "Lock a slot"],
+	"Paralyze" : ["Percent chance of missing attack", "Discard a card"],
+	"Weaken" : ["Reduce hand size", "Reduce damage"],
+	"Health" : ["", "Extra health"],
+	"Word" : ["", "Extra Word Length"],
+	"Hand" : ["", "Extra Hand Size"],
+}
