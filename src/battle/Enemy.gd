b16a963f95cbbcef486915a873c7bf41ae07f3b6
Evan Lam <45635779+IMlame@users.noreply.github.com>
2022-09-24 10:19:17 -0700

    enemy enums

--- src/battle/Enemy.gd
+++ src/battle/Enemy.gd
@@ -1,14 +1,18 @@
 extends Sprite
 
-enum ENEMY_TYPE {NORMAL, AXOLOTL, BULLY, FRIDGE}
+enum ENEMY_TYPES {BASIC, SHIELD, BROILER, COOL, AXOLOTL, FRIDGE}
 
-var enemy = ENEMY_TYPE.NORMAL
+enum EFFECT_TYPES {FREEZE, BURN, WEAKNESS, PARALYZE, HEAL}
+
+var enemy = ENEMY_TYPES.BASIC
+
+var effects = {EFFECT_TYPES.FREEZE: 0, EFFECT_TYPES.BURN: 0, EFFECT_TYPES.WEAKNESS: 0, EFFECT_TYPES.PARALYZE: 0, EFFECT_TYPES.HEAL: 0}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 	
-func _setup_enemy(enemy: int, max_health: int, base_damage: int, effects: Array, immunities: Array):
+func _setup_enemy(type: int, max_health: int, base_damage: int, effects: Array, immunities: Array):
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
